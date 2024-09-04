fb1177052a0e29680d85679aa857dcc22d7b6f55
Rômulo Cibien <romulo.cibien@hotmail.com.br>
2024-09-03 22:56:09 -0300

    feat: add docker compose with database and challenge procedure

---
+++ create_table.sql
@@ -0,0 +1,49 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+
+---
+--- drop tables
+---
+
+
+DROP TABLE IF EXISTS clients;
+DROP TABLE IF EXISTS transactions;
+
+CREATE TABLE IF NOT EXISTS clients (
+    id SERIAL PRIMARY KEY NOT NULL,
+    limite INTEGER NOT NULL,
+    saldo INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id SERIAL PRIMARY KEY NOT NULL,
+    tipo CHAR(1) NOT NULL,
+    descricao VARCHAR(10) NOT NULL,
+    valor INTEGER NOT NULL,
+    cliente_id INTEGER NOT NULL,
+    realizada_em TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+INSERT INTO clients (limite, saldo)
+VALUES
+    (100000, 0),
+    (80000, 0),
+    (1000000, 0),
+    (10000000, 0),
+    (500000, 0);
+
